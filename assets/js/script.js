f4d3156ccac4b06b178bb3c16dbbc9aac6cc364e
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-08-05 15:51:47 +0700

    add profile, time and edit size app in larger screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,54 @@
+const timeWidget = () => {
+  const dateWidget = document.getElementById("dateWidget");
+  const timeWidget = document.getElementById("timeWidget");
+
+  const updateTime = () => {
+    const today = new Date();
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    dateWidget.textContent = today.toLocaleDateString("en-US", options);
+    timeWidget.textContent = today.toLocaleTimeString("en-US", {
+      hour12: false,
+    });
+  };
+
+  updateTime();
+  const intervalId = setInterval(updateTime, 1000);
+  return () => clearInterval(intervalId);
+};
+
 const addTodo = (e) => {
   e.preventDefault();
 
-  timeNow = new Date().toLocaleString();
-
   const todoList = document.getElementById("todoList");
-  const todo = document.getElementById("inputTodo").value;
+  const todo = document.getElementById("inputTodo").value.trim();
   const priority = document.getElementById("inputPriority").value;
   const date = document.getElementById("inputDate").value;
 
   if (!todo || !priority || !date) {
-    alert("Data belum lengkap");
+    const errors = [];
+    if (!todo) errors.push("Kegiatan is required");
+    if (!priority) errors.push("Priority is required");
+    if (!date) errors.push("Date is required");
+    alert(errors.join("\n"));
     return;
   }
 
   const dataTodo = {
     id: Date.now(),
     todo: todo,
-    date: timeNow,
+    date,
     priority: priority,
     status: "todo",
   };
   /*
   todo list on another pages/modal
   todo create modal for form input
-  todo create profile and show time
+  todo style profile and show time
   todo create display priority
   todo styling the entire app
   */
@@ -32,14 +57,98 @@ const addTodo = (e) => {
   todos.push(dataTodo);
   localStorage.setItem("todos", JSON.stringify(todos));
 
-  loadingIndicator.style.display = "none";
-
   todoList.innerHTML = "";
 
   document.getElementById("inputTodo").value = "";
   document.getElementById("inputPriority").value = "";
+  document.getElementById("inputDate").value = "";
 
   loadTodoList();
+};
+
+const loadTodoList = () => {
+  const todoList = document.getElementById("todoList");
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  const todoItems = todos.filter((todo) => todo.status === "todo");
+
+  todoList.innerHTML = "";
+
+  if (todoItems.length === 0) {
+    todoList.innerHTML = `<p class="emptyData">Tidak ada data</p>`;
+  } else {
+    todoItems.forEach((todo) => {
+      if (todo.status === "todo") {
+        const cardTodoElement = document.createElement("div");
+        cardTodoElement.classList.add("card-todo");
+        cardTodoElement.setAttribute("data-index", todo.id);
+        cardTodoElement.innerHTML = `
+        <div class="card-content">
+          <p class="card-todo-desc" style="${
+            todo.status === "done" ? "text-decoration: line-through;" : ""
+          }">${todo.todo}</p>
+        </div>
+        <div class="card-todo-footer">
+          <span class="card-todo-time">${todo.date}</span>
+          <span class="card-todo-priority">${todo.priority}</span>
+          <button class="card-todo-delete">🗑</button>
+          <input type="checkbox" class="card-todo-checkbox" ${
+            todo.status === "done" ? "checked" : ""
+          }>
+        </div>
+    `;
+        todoList.appendChild(cardTodoElement);
+        const deleteBtn = cardTodoElement.querySelector(".card-todo-delete");
+        deleteBtn.addEventListener("click", () => deleteTodo(todo.id));
+      }
+    });
+  }
+
+  todoList.removeEventListener("change", handleCheckboxChange);
+  todoList.addEventListener("change", handleCheckboxChange);
+};
+
+const loadDoneList = () => {
+  const doneList = document.getElementById("doneList");
+
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  const filteredList = todos.filter((todo) => todo.status === "done");
+
+  doneList.innerHTML = "";
+
+  if (filteredList.length === 0) {
+    doneList.innerHTML = `<p class="emptyData">Tidak ada data</p>`;
+  } else {
+    filteredList.forEach((todo) => {
+      const cardTodoElement = document.createElement("div");
+      cardTodoElement.classList.add("card-todo");
+      cardTodoElement.setAttribute("data-index", todo.id);
+      cardTodoElement.innerHTML = `
+      <div class="card-content">
+        <p class="card-todo-desc" style="${
+          todo.status === "done" ? "text-decoration: line-through;" : ""
+        }">${todo.todo}</p>
+      </div>
+      <div class="card-todo-footer">
+        <span class="card-todo-time">${todo.date}</span>
+        <span class="card-todo-priority">${todo.priority}</span>
+        <input type="checkbox" class="card-todo-checkbox" ${
+          todo.status === "done" ? "checked" : ""
+        }>
+      </div>
+  `;
+      doneList.appendChild(cardTodoElement);
+    });
+  }
+
+  doneList.removeEventListener("change", handleCheckboxChange);
+  doneList.addEventListener("change", handleCheckboxChange);
+};
+
+const handleAddTodo = () => {
+  const todoForm = document.getElementById("todoForm");
+  todoForm.addEventListener("submit", (e) => addTodo(e));
 };
 
 const handleCheckboxChange = (e) => {
@@ -47,6 +156,7 @@ const handleCheckboxChange = (e) => {
     const checkbox = e.target;
     const todoCard = checkbox.closest(".card-todo");
     const index = parseInt(todoCard.getAttribute("data-index"), 10);
+    const lineThrough = todoCard.querySelector(".card-todo-desc");
 
     let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -56,8 +166,10 @@ const handleCheckboxChange = (e) => {
     if (todoIndex !== -1) {
       if (checkbox.checked) {
         todos[todoIndex].status = "done";
+        lineThrough.style.textDecoration = "line-through";
       } else {
         todos[todoIndex].status = "todo";
+        lineThrough.style.textDecoration = "none";
       }
 
       localStorage.setItem("todos", JSON.stringify(todos));
@@ -89,85 +201,6 @@ const handleDeleteAll = () => {
   });
 };
 
-const loadTodoList = () => {
-  const todoList = document.getElementById("todoList");
-
-  const todos = JSON.parse(localStorage.getItem("todos")) || [];
-  console.log(todos[0]);
-
-  todoList.innerHTML = "";
-
-  if (todos.length === 0) {
-    todoList.innerHTML = `<p class="emptyData">Tidak ada data</p>`;
-  } else {
-    todos.forEach((todo) => {
-      if (todo.status === "todo") {
-        const cardTodoElement = document.createElement("div");
-        cardTodoElement.classList.add("card-todo");
-        cardTodoElement.setAttribute("data-index", todo.id);
-        cardTodoElement.innerHTML = `
-        <div class="card-content">
-          <p class="card-todo-desc">${todo.todo}</p>
-        </div>
-        <div class="card-todo-footer">
-          <span class="card-todo-time">${todo.date}</span>
-          <span class="card-todo-priority">${todo.priority}</span>
-          <button class="card-todo-delete">Del</button>
-          <input type="checkbox" class="card-todo-checkbox" ${
-            todo.status === "done" ? "checked" : ""
-          }>
-        </div>
-    `;
-        todoList.appendChild(cardTodoElement);
-        const deleteBtn = cardTodoElement.querySelector(".card-todo-delete");
-        deleteBtn.addEventListener("click", () => deleteTodo(todo.id));
-      }
-    });
-  }
-
-  todoList.addEventListener("change", handleCheckboxChange);
-};
-
-const loadDoneList = () => {
-  const doneList = document.getElementById("doneList");
-
-  const todos = JSON.parse(localStorage.getItem("todos")) || [];
-
-  const filteredList = todos.filter((todo) => todo.status === "done");
-
-  doneList.innerHTML = "";
-
-  if (filteredList.length === 0) {
-    doneList.innerHTML = `<p class="emptyData">Tidak ada data</p>`;
-  } else {
-    filteredList.forEach((todo) => {
-      const cardTodoElement = document.createElement("div");
-      cardTodoElement.classList.add("card-todo");
-      cardTodoElement.setAttribute("data-index", todo.id);
-      cardTodoElement.innerHTML = `
-      <div class="card-content">
-        <p class="card-todo-desc">${todo.todo}</p>
-      </div>
-      <div class="card-todo-footer">
-        <span class="card-todo-time">${todo.date}</span>
-        <span class="card-todo-priority">${todo.priority}</span>
-        <input type="checkbox" class="card-todo-checkbox" ${
-          todo.status === "done" ? "checked" : ""
-        }>
-      </div>
-  `;
-      doneList.appendChild(cardTodoElement);
-    });
-  }
-
-  doneList.addEventListener("change", handleCheckboxChange);
-};
-
-const handleAddTodo = () => {
-  const todoForm = document.getElementById("todoForm");
-  todoForm.addEventListener("submit", addTodo);
-};
-
 const handleChangeSection = () => {
   const btnTodoList = document.getElementById("btnTodoList");
   const btnDoneList = document.getElementById("btnDoneList");
@@ -191,4 +224,9 @@ const handleChangeSection = () => {
 
 handleChangeSection();
 handleAddTodo();
-window.onload = loadTodoList;
+handleDeleteAll();
+window.onload = () => {
+  document.getElementById("doneList").style.display = "none";
+  loadTodoList();
+  timeWidget();
+};
